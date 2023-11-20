4e7ad2c92f1d5022bf239342c6aa3bdb132334e0
Théo Damiani <theo.damiani@epfl.ch>
2023-11-20 18:54:41 +0100

    fix: infinte ruler system + add fade out to the Ice plane

--- Assets/Simulation/InfiniteTimeLine/InfiniteTimeLine.cs
+++ Assets/Simulation/InfiniteTimeLine/InfiniteTimeLine.cs
@@ -14,6 +14,7 @@ public class InfiniteTimeLine : MonoBehaviour
     private Vector3 lineDirectionNormalized;
     private int indexParticleSet = 0;
     private float lineHalfSize;
+    private Vector3 lineHalfSizeVector3;
     private float lineHalfSizeSqr;
     private Rigidbody parentRigidbody;
 
@@ -30,7 +31,10 @@ public class InfiniteTimeLine : MonoBehaviour
             nbOfMarkers++; // round to odd number so first marker is at tx position;
         }
         lineHalfSize = markersOffset*(nbOfMarkers-1)/2;
+        lineHalfSizeVector3 = lineDirectionNormalized*lineHalfSize;
         lineHalfSizeSqr = (lineHalfSize*lineDirectionNormalized).sqrMagnitude;
+
+        nbOfMarkers--; // we do not want to add a marker at the top left position on the line.
 
         var psMain = GetComponent<ParticleSystem>().main;
         psMain.startSize3D = true;
@@ -55,15 +59,20 @@ public class InfiniteTimeLine : MonoBehaviour
         markers = new ParticleSystem.Particle[nbOfMarkers];
         for (int i = 0; i < nbOfMarkers; i++) {
             markers[i].position = transform.position + (lineHalfSize * -lineDirectionNormalized) + (markersOffset * i * lineDirectionNormalized);
+            //markers[i].position += lineDirectionNormalized*0.1f;
             markers[i].startSize3D = new Vector3(1f,0.1f,0.1f);
             // Marker are oriented such that their forward (blue) axis are along the line direction;
             markers[i].rotation3D = rotationTowardsLineDirection;
-            //markers[i].startColor = new Color(1,1,1, 1);
+            markers[i].startColor = new Color(1,1,1,1);
             //markers[i].startSize = markerSize;
         }
         indexParticleSet = nbOfMarkers-1;
         GetComponent<ParticleSystem>().SetParticles ( markers, markers.Length );
+
+        markerColorUpdate = false;
     }
+
+    private bool markerColorUpdate = false;
 
     void FixedUpdate () 
     {
@@ -71,15 +80,35 @@ public class InfiniteTimeLine : MonoBehaviour
         {
             indexParticleSet = nbOfMarkers-1;
         }
-        //Vector3 lineNorm = parentRigidbody.velocity.normalized;
-        if((markers[indexParticleSet].position - transform.position).sqrMagnitude > lineHalfSizeSqr) 
+
+        Vector3 distance = markers[indexParticleSet].position - transform.position;
+        //if(-(distance.sqrMagnitude - lineHalfSizeSqr) < 0.1f )
+        if ((distance - lineHalfSizeVector3).x <= 0)
         {
             markers[indexParticleSet].position = transform.position + (lineHalfSize * -lineDirectionNormalized);
+            markers[indexParticleSet].position += distance - lineHalfSizeVector3; // correct the position, because of the precision of FixedUpdate
 
             indexParticleSet--;
             GetComponent<ParticleSystem>().SetParticles ( markers, markers.Length );
         }
         
+        for (int i = 0; i < nbOfMarkers; i++) {
+ 
+            distance = markers[i].position - transform.position;
 
+            if (distance.sqrMagnitude <= lineHalfSizeSqr) 
+            {
+                float percent = (distance.sqrMagnitude) / (lineHalfSizeSqr);
+                markers[i].startColor = new Color(1,1,1, 1-percent);
+
+                markerColorUpdate = true;
+            }
+        }
+
+        if (markerColorUpdate)
+        {
+            GetComponent<ParticleSystem>().SetParticles ( markers, markers.Length );
+            markerColorUpdate = false;
+        }
     }
 }
