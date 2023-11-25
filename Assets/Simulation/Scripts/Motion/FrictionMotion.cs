cd7e07300e9269bf9ce5fd6c27bf02555fdcab80
Théo Damiani <theo.damiani@epfl.ch>
2023-11-25 15:22:36 +0100

    fix: bidirectionnal friction

--- Assets/Simulation/Scripts/Motion/FrictionMotion.cs
+++ Assets/Simulation/Scripts/Motion/FrictionMotion.cs
@@ -15,17 +15,23 @@ public class FrictionMotion : Motion
     [SerializeField] private BoolReference currentFrictionCoeff; // 0 for static, 1 for kinetic
     [SerializeField] private Vector3Reference currentMaxFriction;
     private Vector3 appliedForceNorm;
+    private float previousVelocitySign;
 
     public override void InitMotion(Rigidbody rigidbody)
     {
         appliedForceNorm = appliedForceOnObject.Value.normalized;
+        previousVelocitySign = Sign(rigidbody.velocity.x);
     }
     public override void ApplyMotion(Rigidbody rigidbody)
     {
-        if (rigidbody.velocity.x < 0)
+        if (previousVelocitySign!=0)
         {
-            rigidbody.velocity = Vector3.zero;
+            if (Sign(rigidbody.velocity.x) != previousVelocitySign)
+            {
+                rigidbody.velocity = Vector3.zero;
+            }
         }
+
         if (rigidbody.velocity.sqrMagnitude == 0f)
         {
             Vector3 staticFrictionForce = frictionStaticCoeff.Value * objectMass.Value * Physics.gravity.y * appliedForceNorm;
@@ -49,14 +55,16 @@ public class FrictionMotion : Motion
         }
         else
         {
-            Vector3 kineticFrictionForce = frictionKineticCoeff.Value * objectMass.Value * Physics.gravity.y * appliedForceNorm;
-            //Vector3 kineticFrictionForce = frictionKineticCoeff.Value * objectMass.Value * Physics.gravity.y * rigidbody.velocity.normalized;
+            //Vector3 kineticFrictionForce = frictionKineticCoeff.Value * objectMass.Value * Physics.gravity.y * appliedForceNorm;
+            Vector3 kineticFrictionForce = frictionKineticCoeff.Value * objectMass.Value * Physics.gravity.y * rigidbody.velocity.normalized;
             // KINETIC FRICTION
             rigidbody.AddForce(kineticFrictionForce, ForceMode.Force);
             SetVectorRepresentation(currentFriction, kineticFrictionForce);
             SetVectorRepresentation(currentMaxFriction, kineticFrictionForce);
             SetFrictionFlag(true);
         }
+
+        previousVelocitySign = Sign(rigidbody.velocity.x);
     }
 
     public void SetVectorRepresentation(Vector3Reference vectorRef, Vector3 newComponents)
@@ -75,5 +83,10 @@ public class FrictionMotion : Motion
             return;
         }
         currentFrictionCoeff.Value = newVal;
+    }
+
+    private float Sign(float number) 
+    {
+        return number < 0 ? -1 : (number > 0 ? 1 : 0);
     }
 }
