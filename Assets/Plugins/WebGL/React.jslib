e661cbf91e13af3020cd4f4b90754f93923f72f6
Théo Damiani <theo.damiani@epfl.ch>
2023-11-27 17:05:44 +0100

    fix: list of data holder can be serialiazed to Json

--- Assets/Plugins/WebGL/React.jslib
+++ Assets/Plugins/WebGL/React.jslib
@@ -1,7 +1,7 @@
 mergeInto(LibraryManager.library, {
   GameObjectDataRecordingDone: function (dataJson) {
     try {
-      window.dispatchReactUnityEvent("SetScore", UTF8ToString(dataJson));
+      window.dispatchReactUnityEvent("GameObjectDataRecordingDone", UTF8ToString(dataJson));
     } catch (e) {
       console.warn("Failed to dispatch event");
     }
--- Assets/Simulation/Scripts/DataExporter.cs
+++ Assets/Simulation/Scripts/DataExporter.cs
@@ -9,33 +9,37 @@ namespace System.Runtime.CompilerServices
     internal static class IsExternalInit {}
 }
 
+
 [Serializable]
-public readonly struct DataHolder
+public class JsonableListWrapper<T>
+{
+    public List<T> list;
+    public JsonableListWrapper(List<T> list) => this.list = list;
+}
+ 
+
+[Serializable]
+public struct DataHolder
 {
     public DataHolder(float t, Vector3 p, Vector3 r, Vector3 v)
     {
-        Time = t;
-        Position = p;
-        Rotation = r;
-        Velocity = v;
+        time = t;
+        position = p;
+        rotation = r;
+        velocity = v;
     }
-    public float Time {get; init;}
-    public Vector3 Position {get; init;}
-    public Vector3 Rotation {get; init;}
-    public Vector3 Velocity {get; init;}
-
-    public override string ToString()
-    {
-        return "Time: " + Time + " | Position: " + Position + " | Rotation: " + Rotation + " | Velocity: " + Velocity;
-    }
+    public float time;
+    public Vector3 position;
+    public Vector3 rotation;
+    public Vector3 velocity;
 }
 
-[RequireComponent(typeof(Rigidbody))]
 public class DataExporter : MonoBehaviour
 {
     [DllImport("__Internal")]
     private static extern void GameObjectDataRecordingDone (string dataJSON);
 
+    [SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private float timeRate;
     [SerializeField] private int maxSize;
     [SerializeField] private bool savePosition;
@@ -43,24 +47,23 @@ public class DataExporter : MonoBehaviour
     [SerializeField] private bool saveVelocity;
 
     private readonly float defaultTimeRate = 0.1f;
-    private List<DataHolder> dataList;
+    private List<string> listOfJsonData;
     private float timeAtStart;
     private bool isRecording;
 
     void Start()
     {
-        dataList = new List<DataHolder>();
+        listOfJsonData = new List<string>();
 
         // From InvokeRepeating: throw new UnityException("Invoke repeat rate has to be larger than 0.00001F)");
         timeRate = (timeRate < 0.00001F) ? defaultTimeRate : timeRate;
         isRecording = false;
     }
-
     public void StartGameObjectRecording()
     {
         isRecording = true;
         timeAtStart = Time.realtimeSinceStartup;
-        dataList.Clear();
+        listOfJsonData.Clear();
         InvokeRepeating(nameof(SaveData), 0f, timeRate);
     }
 
@@ -70,29 +73,28 @@ public class DataExporter : MonoBehaviour
         CancelInvoke(nameof(SaveData));
 
         #if UNITY_WEBGL == true && UNITY_EDITOR == false
-            GameObjectDataRecordingDone(JsonUtility.ToJson(dataList));
+            GameObjectDataRecordingDone(JsonUtility.ToJson(new JsonableListWrapper<string>(listOfJsonData)));
         #endif
 
-        dataList.Clear();
+        listOfJsonData.Clear();
     }
 
     void SaveData()
     {
-        if (dataList.Count <= maxSize)
+        if (listOfJsonData.Count <= maxSize)
         {
             DataHolder data = new(
                     Time.realtimeSinceStartup - timeAtStart,
                     transform.position,
                     transform.rotation.eulerAngles,
-                    GetComponent<Rigidbody>().velocity
+                    _rigidbody.velocity
                 );
 
-            dataList.Add(data);
+            listOfJsonData.Add(JsonUtility.ToJson(data));
         }
         else
         {
             StopGameObjectRecording();
-            Debug.Log("Recording stopped: ");
         }
     }
 }
